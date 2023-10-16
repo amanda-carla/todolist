7ae06b74ccaba10f384dbeb109ece6e87c342e07
amanda-carla <amandaxcarlasb@gmail.com>
2023-10-16 00:40:10 -0300

    build: adjust dockerfile

---
+++ src/main/java/br/com/amandacarla/todolist/TodolistApplication.java
@@ -0,0 +1,19 @@
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by FernFlower decompiler)
+//
+
+package br.com.amandacarla.todolist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodolistApplication {
+    public TodolistApplication() {
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(TodolistApplication.class, args);
+    }
+}
